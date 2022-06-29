80f94784ec94e38430b1b968bf6d467267ff22f1
Vladsdsh <dissakesamuel@gmail.com>
2022-06-29 10:08:04 +0200

    Python file create

---
+++ Python/Rock Paper Scissors.py
@@ -0,0 +1,8 @@
+Rock Paper Scissors : https://www.codewars.com/kata/5672a98bdbdd995fad00000f/python
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output) :
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
--- Rock Paper Scissors.py
+++
@@ -1,4 +0,0 @@
-Rock Paper Scissors
-Let's play! You have to return which player won! In case of a draw return Draw!.
-
-Examples(Input1, Input2 --> Output):
