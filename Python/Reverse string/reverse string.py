ddcdb4f893e94e58f7c353f3e81ac5fefb201732
Vladsdsh <dissakesamuel@gmail.com>
2022-07-10 11:45:38 +0200

    add refact

---
+++ Python/Reverse string/reverse string.py
@@ -0,0 +1,4 @@
+#for reverse string we use the method [start:stop:step]
+#Complete the solution so that it reverses the string passed into it.
+#https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
