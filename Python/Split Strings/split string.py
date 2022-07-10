aa956a0c414b7514711e824f09f89c6b8d0cb4c6
Vladsdsh <dissakesamuel@gmail.com>
2022-07-10 11:45:52 +0200

    split test

--- Python/Split Strings/split string.py
+++ Python/Split Strings/split string.py
@@ -1,7 +1,7 @@
 #Complete the solution so that it splits the string into pairs of two characters. 
 #f the string contains an odd number of characters then it should replace the missing 
 #second character of the final pair with an underscore ('_').
-
+#https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/python
 
 def solution(s):  
     if len(s)<=1:
@@ -10,3 +10,8 @@ def solution(s):
         else:
             return [s+'_']
     return [s[0:2]]+solution(s[2:])
+
+#refact code 
+import re
+def solution(s):
+    return re.findall(".{2}", s + "_")
