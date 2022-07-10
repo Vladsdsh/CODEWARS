9b7ed96eab3d8fde42bb3af837fce59d7bb39b90
Vladsdsh <dissakesamuel@gmail.com>
2022-07-10 11:58:32 +0200

    implement solution

--- Python/Reverse string/reverse string.py
+++ Python/Reverse string/reverse string.py
@@ -2,3 +2,5 @@
 #Complete the solution so that it reverses the string passed into it.
 #https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
 
+def solution(str):
+    return str[::-1]
