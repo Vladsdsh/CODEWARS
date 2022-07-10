aa225089ff7fd183b2c9969a9e6aafee2a3ac27a
Vladsdsh <dissakesamuel@gmail.com>
2022-07-10 20:16:35 +0200

    solution

---
+++ Python/Convert number to reversed array of digits.py
@@ -0,0 +1,6 @@
+# task : Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+# Example(Input => Output): 348597 => [7,9,5,8,4,3]; 0 => [0]
+
+def digitize(n):
+    return [int(s) for s in str(n)][::-1]
+    
