fc9cd492813ec0a9bd7a0909d0819daf5a3ea643
Vladsdsh <dissakesamuel@gmail.com>
2022-07-14 00:11:41 +0200

    solution

--- Python/Return Negative.py
+++ Python/Return Negative.py
@@ -7,3 +7,5 @@
 #The number can be negative already, in which case no change is required.
 #Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
+def make_negative( number ):
+    return -abs(number) 
