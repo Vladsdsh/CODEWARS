71bc5fe92d74edb2b47a5da7b392864f4521276c
Vladsdsh <dissakesamuel@gmail.com>
2022-07-16 13:35:59 +0200

    exrecice

---
+++ Python/Counting sheep.py
@@ -0,0 +1,5 @@
+# Considérez un tableau/une liste de moutons où certains moutons peuvent manquer à leur place. 
+# Nous avons besoin d'une fonction qui compte le nombre de moutons présents dans le tableau (vrai signifie présent).
+# Par exemple,
+# https://www.codewars.com/kata/54edbc7200b811e956000556
+
