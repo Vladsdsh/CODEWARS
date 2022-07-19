66611ad79bd7c21eec6a396dfbdc2899f7a16afb
Vladsdsh <dissakesamuel@gmail.com>
2022-07-19 20:03:50 +0200

    exercice

---
+++ SQL/Even or Odd.sql
@@ -0,0 +1,9 @@
+/*SQL Notes:
+You will be given a table, numbers, with one column number.
+
+Return a table with a column is_even containing "Even" or "Odd" depending on number column values.
+
+numbers table schema
+number INT
+output table schema
+is_even STRING */
