1f0a64356d514028959d8cd0da73abe9bd5dff31
Vladsdsh <dissakesamuel@gmail.com>
2022-07-19 18:19:15 +0200

    solution

--- SQL/Opposite number.sql
+++ SQL/Opposite number.sql
@@ -3,5 +3,8 @@ Examples:
 1: -1
 14: -14
 -34: 34
-You will be given a table: opposite, with a column: number. Return a table with a column: res./*
+You will be given a table: opposite, with a column: number. Return a table with a column: res.*/
 
+SELECT
+  NUMBER * (-1) AS res
+FROM OPPOSITE
