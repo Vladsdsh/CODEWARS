42cffe7005e684f0caa8dcba719713840ad1ec3d
Vladsdsh <dissakesamuel@gmail.com>
2022-07-19 18:17:38 +0200

    exercice

---
+++ SQL/Opposite number.sql
@@ -0,0 +1,7 @@
+/*Very simple, given an integer or a floating-point number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+You will be given a table: opposite, with a column: number. Return a table with a column: res./*
+
