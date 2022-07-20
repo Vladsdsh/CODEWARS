eb525ae1a54ddef71f16046c7ecaff0683456fe8
Vladsdsh <dissakesamuel@gmail.com>
2022-07-20 16:41:01 +0200

    solution

--- SQL/GROCERY STORE:Support Local Products.sql
+++ SQL/GROCERY STORE:Support Local Products.sql
@@ -10,3 +10,10 @@ country
 results table schema
 products
 country */
+
+-- Replace with your SQL Query
+SELECT COUNT(*) as products, country
+FROM products p
+WHERE country IN ('United States of America', 'Canada') 
+group by country
+order by products desc
