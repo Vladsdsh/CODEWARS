f10a98672345a99e3d6a91acafcaee6d1203c58c
Vladsdsh <dissakesamuel@gmail.com>
2022-07-27 22:38:01 +0200

    solution

--- Python/Do I get a bonus?.py
+++ Python/Do I get a bonus?.py
@@ -5,3 +5,11 @@ Build a function that takes in two arguments (salary, bonus). Salary will be an 
 If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).'''
+
+
+def bonus_time(salary, bonus):
+    #your code here
+    if bonus :
+        return '$' + str(salary * 10)
+    else:
+        return '$' + str(salary)
