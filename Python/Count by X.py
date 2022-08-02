1bdcbc921ec77d506b75459ec89f56e068af8e04
Vladsdsh <dissakesamuel@gmail.com>
2022-08-02 10:16:25 +0200

    solution

--- Python/Count by X.py
+++ Python/Count by X.py
@@ -10,3 +10,10 @@ Examples:
 count_by(1,10) #should return [1,2,3,4,5,6,7,8,9,10]
 count_by(2,5) #should return [2,4,6,8,10]'''
 
+#refcator
+
+def count_by(x, n): 
+    out = []
+    for i in range(n):
+        out.append(x*(i+1))
+    return out
