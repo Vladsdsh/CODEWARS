706d31df62e2336d8cf2717c81a661f5941eb422
Vladsdsh <dissakesamuel@gmail.com>
2023-10-17 11:03:34 +0200

    soolution by using of with

---
+++ SQL/Top 10 customers by total payments amount.sql
@@ -0,0 +1,35 @@
+--For this kata we will be using the DVD Rental database.
+
+--You are working for a company that wants to reward its top 10 customers with a free gift. You have been asked to generate a simple report that returns the top 10 customers by total amount spent ordered from highest to lowest. Total number of payments has also been requested.
+
+--The query should output the following columns:
+
+--customer_id [int4]
+--email [varchar]
+--ayments_count [int]
+--total_amount [float]
+--and has the following requirements:
+
+--only returns the 10 top customers, ordered by total amount spent from highest to lowest
+
+-- Solution 1 :  used the with
+
+WITH TopCustomers AS (
+    SELECT 
+        c.customer_id,
+        c.email,
+        COUNT(p.payment_id) AS payments_count,
+        CAST(SUM(p.amount) AS FLOAT) AS total_amount
+    FROM 
+        customer c
+    JOIN 
+        payment p ON c.customer_id = p.customer_id
+    GROUP BY 
+        c.customer_id, c.email
+    ORDER BY 
+        total_amount DESC
+    LIMIT 
+        10
+)
+
+SELECT * FROM TopCustomers
