4a5848f837f76dbb92978b4e95d2838edd5f8cbf
Vladsdsh <dissakesamuel@gmail.com>
2023-10-19 12:27:34 +0200

    ici je mets à jour le constructeur avec des entrées input

--- Python/cronstructeur_somme.py
+++ Python/cronstructeur_somme.py
@@ -1,12 +1,24 @@
 class Somme:
-    def __init__(self, n1, n2=0):
+    def __init__(self, n1, n2):
         self.n1 = n1
-        self.n2=n2
-
+        self.n2 = n2
 
     def som(self):
-        return self.n1+(self.n2)
+        return self.n1 + self.n2
+    
+    def div(self):
+        return self.n1 / self.n2
 
-main = Somme(12)
-resultat = main.som()
-print(resultat)
+# Dans la méthode principale (main), demandez à l'utilisateur d'entrer deux entiers
+n1 = int(input("Entrez le premier entier : "))
+n2 = int(input("Entrez le deuxième entier : "))
+
+# Créez une instance de la classe Somme en passant les deux nombres à son constructeur
+calcul = Somme(n1, n2)
+
+# Appelez la méthode som() pour calculer la somme et affichez le résultat
+resultat = calcul.som()
+print(f"La somme de {n1} et {n2} est égale à : {resultat}")
+
+result = calcul.div()
+print(f"La division de {n1} et {n2} est égale à : {result}")
