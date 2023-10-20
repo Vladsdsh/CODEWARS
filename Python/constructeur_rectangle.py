9c50909caea93831f0789cf8c564e98a9357bdc9
Vladsdsh <dissakesamuel@gmail.com>
2023-10-20 21:14:59 +0200

    constructeur d'un rectangle, l'aire et le périmètre

---
+++ Python/constructeur_rectangle.py
@@ -0,0 +1,15 @@
+import math
+
+class rectangle:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur 
+        self.largeur = largeur 
+
+    def aire(self):
+        return (self.longueur * self.largeur)
+    def perimetre(self):
+        return (2*(self.longueur + self.largeur))
+
+tableau = rectangle (12,4)
+print("L'aire du tableau est : ", tableau.aire())
+print("Le périmètre du tableau est : ", tableau.perimetre())
